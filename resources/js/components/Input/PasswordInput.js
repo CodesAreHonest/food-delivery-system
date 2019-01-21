a58c6c050db2c6fd862d24b251fce10d7f91f30a
YingHua Chai <yinghuachai0@gmail.com>
2019-01-21 21:06:26 +0800

    login layout complete without register feature

---
+++ resources/js/components/Input/PasswordInput.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import {Input} from 'reactstrap';
+import PropTypes from 'prop-types';
+
+class PasswordInput extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            valid: false,
+        }
+    }
+
+    render() {
+        return (
+            <Input
+                type="password"
+                id={this.props.id}
+                name={this.props.name}
+                className={this.props.className}
+                placeholder={this.props.placeholder}
+                required={this.props.required}
+                disabled={this.props.disabled}
+
+                valid={this.state.valid}
+            />
+        )
+    }
+}
+
+export default PasswordInput;
+
+PasswordInput.defaultProps = {
+    required: false,
+    disabled: false
+};
+
+PasswordInput.propTypes = {
+    name: PropTypes.string,
+    id: PropTypes.string,
+    placeholder: PropTypes.string,
+    required: PropTypes.bool,
+    disabled: PropTypes.bool
+};
+
+
+
